56b41790d75f15ada02dac61542129112424d4ce
adeisaac7 <140983113+adeisaac7@users.noreply.github.com>
2024-11-30 02:06:02 +0100

    up

--- src/styles.js
+++ src/styles.js
@@ -4,18 +4,14 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-  "font-extrabold text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-
+    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
   heroSubText:
-    "text-[#ffeded] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
   sectionHeadText:
-  "text-white font-extrabold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
-  "sm:text-[18px] text-[14px] text-[#d9f0ff] uppercase tracking-wider",
-
-  buttonHoverEffect:
-  "transition-all ease-in-out duration-300 hover:scale-105 hover:bg-opacity-80",
+    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
 
 export { styles };
